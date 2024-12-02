6f67d63866ec82f17b2d9e708650efd318a82822
Christopher Ly <ckl230001@utdallas.edu>
2024-12-02 16:36:12 -0600

    Created pseudocode and created a template for important classes and functions.

--- src/project.py
+++ src/project.py
@@ -1,18 +1,35 @@
+# Import pygame, random, and math libraries
+# Set the game resolution
+# Create main function for game
+# Import images for game assets
+# Create a class for fish AI
+# Create movement for fish AI
+# Use image asset for fish AI
+# Create a class for fish player
+# Create player controller for fish player
+# Use image asset for fish player
+# Check if player collects food
+# Create a class for fish food
+# Create a function for score display of food collected
+# Create a function to create the aquarium background
+
+class Fish:
+    ...
+
+class PlayerFish:
+    ...
+
+class Food:
+    ...
+
+def draw_aquarium():
+    ...
+
+def display_score():
+    ...
+
 def main():
     ...
-
-
-def function_1():
-    ...
-
-
-def function_2():
-    ...
-
-
-def function_n():
-    ...
-
 
 if __name__ == "__main__":
     main()
